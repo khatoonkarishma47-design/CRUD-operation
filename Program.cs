ce377ba1a3a92ae9044ad9cc40bcdcb37f203dc3
Karishma Khatoon <Karishma.Khatoon@einfochips.com>
2026-03-26 12:34:06 +0530

    Fix JSON circular reference issue for orders

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ using ProductService.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 // Configure Entity Framework
 var useInMemoryDb = builder.Configuration.GetValue<bool>("UseInMemoryDatabase");
